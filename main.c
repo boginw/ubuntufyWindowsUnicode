d409a19cbe8441adc4afd649f689a06ae36f8746
boginw <bogi.wennerstrom@gmail.com>
2017-12-25 18:01:34 +0100

    Free console

--- main.c
+++ main.c
@@ -46,22 +46,24 @@ bool KeyUp(int code);
  * @return exit
  */
 int main(){
- 	HINSTANCE instance = GetModuleHandle(NULL);
-	// Install the low-level keyboard & mouse hooks
-	HHOOK hhkLowLevelKybd = 
-		SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, instance, 0);
+    FreeConsole();
 
-	// Keep this app running until we're told to stop
-	MSG msg;
-	while (!GetMessage(&msg, 0, 0, 0)) {	//this while loop keeps the hook
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+    HINSTANCE instance = GetModuleHandle(NULL);
+    // Install the low-level keyboard & mouse hooks
+    HHOOK hhkLowLevelKybd = 
+        SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, instance, 0);
 
-	UnhookWindowsHookEx(hhkLowLevelKybd);
+    // Keep this app running until we're told to stop
+    MSG msg;
+    while (!GetMessage(&msg, 0, 0, 0)) {    //this while loop keeps the hook
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	// Exit normally
-	return 0;
+    UnhookWindowsHookEx(hhkLowLevelKybd);
+
+    // Exit normally
+    return 0;
 }
 
 /**
@@ -73,28 +75,28 @@ int main(){
  *                returned by CallNextHookEx
  */
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam){
-	bool eatStroke = false;
-	
-	// Declare a pointer to the KBDLLHOOKSTRUCTdsad
-	KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *)lParam;
-	int code = pKeyBoard->vkCode;
+    bool eatStroke = false;
+    
+    // Declare a pointer to the KBDLLHOOKSTRUCTdsad
+    KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *)lParam;
+    int code = pKeyBoard->vkCode;
 
-	if (nCode == HC_ACTION){
-		switch (wParam){
-			// Key is down
-			case WM_KEYDOWN:
-			case WM_SYSKEYDOWN:
-				eatStroke = KeyDown(code);
-				break;
-			// Key is up
-			case WM_KEYUP:
-			case WM_SYSKEYUP:
-				eatStroke = KeyUp(code);
-				break;
-		}
-	}
+    if (nCode == HC_ACTION){
+        switch (wParam){
+            // Key is down
+            case WM_KEYDOWN:
+            case WM_SYSKEYDOWN:
+                eatStroke = KeyDown(code);
+                break;
+            // Key is up
+            case WM_KEYUP:
+            case WM_SYSKEYUP:
+                eatStroke = KeyUp(code);
+                break;
+        }
+    }
 
-	return eatStroke ? 1 : CallNextHookEx(NULL, nCode, wParam, lParam);
+    return eatStroke ? 1 : CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
 /**
@@ -103,27 +105,27 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam){
  * @return      Whether or not key should be eaten
  */
 bool KeyDown(int code){
-	// Change key states
-	if(code == CTRL){
-		CTRL_DOWN = true;
-	} else if(code == SHIFT){
-		SHIFT_DOWN = true;
-	} 
-	// Determine if need to eat key
-	else if(CTRL_DOWN && SHIFT_DOWN && code == U){
-		// user activated unicode input
-		return true;
-	} else if(ACTIVATED && isHexChar(code)){
-		// user is typing number
-		return true;
-	} else if(ACTIVATED && !CTRL_DOWN && !SHIFT_DOWN 
-			&& !U_DOWN && (code == ENTER || code == SPACE)){
-		// The user pressed ctrl+shift+u then typed number, 
-		// then pressed enter or space
-		return true;
-	}
+    // Change key states
+    if(code == CTRL){
+        CTRL_DOWN = true;
+    } else if(code == SHIFT){
+        SHIFT_DOWN = true;
+    } 
+    // Determine if need to eat key
+    else if(CTRL_DOWN && SHIFT_DOWN && code == U){
+        // user activated unicode input
+        return true;
+    } else if(ACTIVATED && isHexChar(code)){
+        // user is typing number
+        return true;
+    } else if(ACTIVATED && !CTRL_DOWN && !SHIFT_DOWN 
+            && !U_DOWN && (code == ENTER || code == SPACE)){
+        // The user pressed ctrl+shift+u then typed number, 
+        // then pressed enter or space
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 /**
@@ -132,69 +134,69 @@ bool KeyDown(int code){
  * @return      Whether or not key should be eaten
  */
 bool KeyUp(int code){
-	if(code == CTRL){
-		CTRL_DOWN = false;
+    if(code == CTRL){
+        CTRL_DOWN = false;
 
-		// If user held ctrl+shift while typing number
-		// the char should be sent on release
-		if(ACTIVATED && SHIFT_DOWN && SENDCHAR > 0){
-			PublishAndReset();
-		}
-	} else if(code == SHIFT){
-		SHIFT_DOWN = false;
+        // If user held ctrl+shift while typing number
+        // the char should be sent on release
+        if(ACTIVATED && SHIFT_DOWN && SENDCHAR > 0){
+            PublishAndReset();
+        }
+    } else if(code == SHIFT){
+        SHIFT_DOWN = false;
 
-		// If user held ctrl+shift while typing number
-		// the char should be sent on release
-		if(ACTIVATED && CTRL_DOWN && SENDCHAR > 0){
-			PublishAndReset();
-		}
-	} 
-	// User activated unicode input
-	else if(CTRL_DOWN && SHIFT_DOWN && code == U){
-		ACTIVATED = true;
-		return true;
-	} 
-	// User typed ctrl+shift+u, then released all keys, then typed number
-	// then pressed either enter or space
-	else if(ACTIVATED && !CTRL_DOWN && !SHIFT_DOWN 
-			&& !U_DOWN && (code == ENTER || code == SPACE)){
-		PublishAndReset();
-		return true;
-	} 
-	// User typed ctrl+shift+u and is now typing hex numbers
-	else if(ACTIVATED && isHexChar(code)){
-		// append input
-		SENDCHAR = (SENDCHAR << 4) | decodeHexChar(code);
+        // If user held ctrl+shift while typing number
+        // the char should be sent on release
+        if(ACTIVATED && CTRL_DOWN && SENDCHAR > 0){
+            PublishAndReset();
+        }
+    } 
+    // User activated unicode input
+    else if(CTRL_DOWN && SHIFT_DOWN && code == U){
+        ACTIVATED = true;
+        return true;
+    } 
+    // User typed ctrl+shift+u, then released all keys, then typed number
+    // then pressed either enter or space
+    else if(ACTIVATED && !CTRL_DOWN && !SHIFT_DOWN 
+            && !U_DOWN && (code == ENTER || code == SPACE)){
+        PublishAndReset();
+        return true;
+    } 
+    // User typed ctrl+shift+u and is now typing hex numbers
+    else if(ACTIVATED && isHexChar(code)){
+        // append input
+        SENDCHAR = (SENDCHAR << 4) | decodeHexChar(code);
 
-		// if input is longer than MAXCHARS, just send the char
-		if(SENDCHAR > MAXCHARS){
-			PublishAndReset();
-		}
+        // if input is longer than MAXCHARS, just send the char
+        if(SENDCHAR > MAXCHARS){
+            PublishAndReset();
+        }
 
-		return true;
-	}
-	// User typed something other
-	else if(ACTIVATED){
-		Reset();
-	}
+        return true;
+    }
+    // User typed something other
+    else if(ACTIVATED){
+        Reset();
+    }
 
-	return false;
+    return false;
 }
 
 /**
  * Publishes current char and resets
  */
 void PublishAndReset(){
-	SendKey(SENDCHAR);
-	Reset();
+    SendKey(SENDCHAR);
+    Reset();
 }
 
 /**
  * Resets input
  */
 void Reset(){
-	ACTIVATED = false;
-	SENDCHAR = 0;
+    ACTIVATED = false;
+    SENDCHAR = 0;
 }
 
 /**
@@ -202,20 +204,20 @@ void Reset(){
  * @param key Key to send
  */
 void SendKey(int key){
-	INPUT ip;
-	ip.type = INPUT_KEYBOARD;
-	ip.ki.time = 0;
-	ip.ki.dwExtraInfo = 0;
+    INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
 
-	// Press a unicode "key"
-	ip.ki.dwFlags = KEYEVENTF_UNICODE;
-	ip.ki.wVk = 0;
-	ip.ki.wScan = key;
-	SendInput(1, &ip, sizeof(INPUT));
+    // Press a unicode "key"
+    ip.ki.dwFlags = KEYEVENTF_UNICODE;
+    ip.ki.wVk = 0;
+    ip.ki.wScan = key;
+    SendInput(1, &ip, sizeof(INPUT));
  
-	// Release key
-	ip.ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
-	SendInput(1, &ip, sizeof(INPUT));
+    // Release key
+    ip.ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+    SendInput(1, &ip, sizeof(INPUT));
 }
 
 /**
@@ -224,10 +226,10 @@ void SendKey(int key){
  * @return     Integer of the input
  */
 int decodeHexChar(int key){
-	if(key <= NUMHIGH){
-		return key - (NUMHIGH - 9);
-	}
-	return key - (CHARHIGH - 5) + 10;
+    if(key <= NUMHIGH){
+        return key - (NUMHIGH - 9);
+    }
+    return key - (CHARHIGH - 5) + 10;
 }
 
 /**
@@ -236,5 +238,5 @@ int decodeHexChar(int key){
  * @return     Whether or not key is hex
  */
 bool isHexChar(int key){
-	return (key >= NUMLOW && key <= NUMHIGH) || (key >= CHARLOW && key <= CHARHIGH);
+    return (key >= NUMLOW && key <= NUMHIGH) || (key >= CHARLOW && key <= CHARHIGH);
 }
